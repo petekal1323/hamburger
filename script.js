da35910f63a87fb155b5643e4910080a2fa7aeb4
Peter Kalinowski <pkalinowski@DecypherCorp.local>
2019-08-19 11:05:09 -0400

    refresh state of hamburger at breakPoint

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-$(document).ready(function(){
-    $(".menu-icon").click(function(){
+$(document).ready(function() {
+    $(".menu-icon").click(function() {
         $(".menu-icon").toggleClass('active');
     });
-    $(".menu-icon").click(function(){
+    $(".menu-icon").click(function() {
         $(".menu").toggleClass('active');
     });
-    
+
+
+    $(window).resize(function() {
+        if ($(window).width() >= 768) {
+            $(".menu-icon").removeClass('active');
+            $(".menu").removeClass('active');
+        }
+    });
+
 });
